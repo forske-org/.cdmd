12eb996b8a1f00cb5a9249b3ac83d25af3a704ad
Manuel Nielsen <MaNielsen@forske.org>
2024-08-23 12:34:08 +1000

    Created omop barrel exports

---
+++ src/omop/_meta.ts
@@ -0,0 +1,7 @@
+import { date } from '../_extended_generic'
+
+export declare type Core = {
+    valid_start_date: date
+    valid_end_Date: date
+    invalid_reason: string
+}
---
+++ src/omop/index.ts
@@ -0,0 +1,8 @@
+export * as Meta from './_meta'
+
+export * as Clinical from './clinical'
+export * as Derived from './derived'
+export * as Economics from './economics'
+export * as HealthSystem from './health_system'
+export * as Metadata from './metadata'
+export * as Vocabulary from './vocabulary'
