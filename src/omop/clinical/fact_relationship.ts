2d78cdfa31650e5bb2e413652c42f675127ac448
Manuel Nielsen <MaNielsen@forske.org>
2024-08-23 12:33:27 +1000

    Created clinical placeholders v5.4

---
+++ src/omop/clinical/fact_relationship.ts
@@ -0,0 +1,7 @@
+export declare type FactRelationship = {
+    domain_concept_id_1: number
+    fact_id_1: number
+    domain_concept_2: number
+    fact_id_2: number
+    relationhip_concept_id: number
+}
---
+++ src/omop/clinical/specimen.ts
@@ -0,0 +1,3 @@
+export declare type Specimen = {
+
+}
